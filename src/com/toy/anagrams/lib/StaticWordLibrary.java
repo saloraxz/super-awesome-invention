b15dbd2ceecea69e5cea1d04da9a0ea61c4fccf1
memesggopoop28 <108966506+memesggopoop28@users.noreply.github.com>
2022-07-10 09:18:38 +0800

    Added a class for word list

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -1,1 +1,12 @@
 /* Anagram Game Application */
+
+package com.toy.anagrams.lib;
+/**
+ * Implementation of the logic for the Anagram Game application.
+ */
+final class StaticWordLibrary extends WordLibrary {
+  
+  private static final String[] WORD_LIST = {
+    
+  }
+}
