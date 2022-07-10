ef3cd72518b243183c9b92bd14f9a2c537b91286
memesggopoop28 <108966506+memesggopoop28@users.noreply.github.com>
2022-07-10 09:05:53 +0800

    Create WordLibrary.java
    14% Done

---
+++ src/com/toy/anagrams/lib/WordLibrary.java
@@ -0,0 +1,15 @@
+/* Anagram Game Application */
+
+package com.toy.anagrams.lib;
+
+/**
+ * Interface defining logic for the Anagram Game Application.
+ */
+public abstract class WordLibrary {
+  /**
+   * Constructor for subclasses.
+   */
+  protected WordLibrary() {
+  }
+  
+}
